50f977361100b93c6fba604654cf83377c8a5e94
Sergio <sergamer15@gmail.com>
2023-02-24 12:07:37 +0100

    Added first test component

---
+++ src/components/Greeting/Greeting.js
@@ -0,0 +1,7 @@
+export const Greeting = () => {
+    return <h1>Primer componente</h1>
+}
+
+export const MiName = () => {
+    return <h3>¡Hola! Mi nombre es PokeSer</h3>
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Greeting, MiName } from "./components/Greeting/Greeting";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<h1>Hola Mundo</h1>)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <>
+    <Greeting />
+    <MiName />
+  </>
+);
